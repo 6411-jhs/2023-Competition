dbbae7aa5108d2042657df60309a2638fb4d70a0
Umbra <carsonjesseangell@gmail.com>
2023-01-21 13:46:16 -0700

    Added data calculator for more accurate observations

--- src/main/java/frc/robot/LimeLight/LimeLight.java
+++ src/main/java/frc/robot/LimeLight/LimeLight.java
@@ -2,11 +2,14 @@ package frc.robot.LimeLight;
 
 // import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+
+import java.util.ArrayList;
 
 public class LimeLight extends SubsystemBase {
    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
@@ -14,18 +17,34 @@ public class LimeLight extends SubsystemBase {
    NetworkTableEntry ty = table.getEntry("ty");
    NetworkTableEntry ta = table.getEntry("ta");
    double x, y, area;
+   double avrg[] = {50.0,0.0,-50.0};
+   ArrayList<Double> data = new ArrayList<Double>();
 
    public LimeLight(){}
 
    public void update(){
+      double sum = 0;
       x = tx.getDouble(0.0);
       y = ty.getDouble(0.0);
       area = ta.getDouble(0.0);
       SmartDashboard.putNumber("LimelightX", x);
       SmartDashboard.putNumber("LimelightY", y);
       SmartDashboard.putNumber("LimelightArea", area);
-      System.out.println(x + ", " + y + ", " + area);
+      data.add(x);
+      if (x < avrg[0]) avrg[0] = x;
+      if (y > avrg[2]) avrg[2] = y;
+      for (int i = 0; i < data.size(); i++){
+         sum += data.get(i);
+      }
+      sum = sum / data.size();
+      System.out.println(avrg[0] + ", " + avrg[1] + ", " + avrg[2]);
    }
+
+   private CommandBase follow(){
+      return runOnce(() -> {
+
+      });
+   };
 
    @Override
    public void periodic(){
