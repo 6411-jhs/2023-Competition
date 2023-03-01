89ce046c41720944541e8322f830d2ee9b42445d
Umbra <carsonjesseangell@gmail.com>
2023-02-28 23:18:58 -0700

    Reprogrammed chargin station code and added notes for what to do the next day

--- src/main/java/frc/robot/commands/EngageChargingStation.java
+++ src/main/java/frc/robot/commands/EngageChargingStation.java
@@ -2,15 +2,12 @@ package frc.robot.commands;
 
 import java.util.ArrayList;
 
-import javax.swing.text.StyleContext.SmallAttributeSet;
-
 import edu.wpi.first.wpilibj.BuiltInAccelerometer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.interfaces.Accelerometer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.DriveTrain;
-import limelightvision.limelight.frc.ControlMode.CamMode;
 
 public class EngageChargingStation extends CommandBase {
    private Accelerometer accelerometer = new BuiltInAccelerometer();
@@ -19,18 +16,38 @@ public class EngageChargingStation extends CommandBase {
 
    // Local Constants
    private double thresholdRange = 0.027;
-   private int sampleSize = 5;
    private double targetIncline = 0.96;
    private double targetBalance = 0.99;
 
-   private ArrayList<Boolean> mountSample = new ArrayList<Boolean>();
-   private ArrayList<Boolean> secondMountSample = new ArrayList<Boolean>();
-   private ArrayList<Boolean> balanceSample = new ArrayList<Boolean>();
-   private double zAcc;
+
+   //Data Collection
+   //Holds the z axis acceleration of the robot; gravity is also detected as an accleration
+   private double zAcc, yAcc;
+
+   //These variables hold the data for mounting, balancing and direction sensing data
+   //Since accelerometer values are extremely inconsistent, the program has to use a different tecnique when it comes to sensing
+   //The program essentially takes the condition of which the bot's accelerometer values matching that of mounting, balancing
+   //and direction targets. When a condition is met it is added to a list to be counted into a DPS (Detections Per Second)
+   //It uses the rate of condition detection to sense whether or not it's balanced, mounted, etc.
+
+   private int mountDPS;
+   private int mountDPSThreshold = 20; //<---- This needs to be set!
+   private ArrayList<Boolean> mountDetections = new ArrayList<Boolean>();
+   private int mountDetectionsLimit = 50;
+
+   private int balanceDPS;
+   private int balanceDPSThreshold = 10; //<----This needs to be set!
+   private ArrayList<Boolean> balanceDetections = new ArrayList<Boolean>();
+   private int balanceDetectionsLimit = 50;
+
+   private int directionDPS;
+   private int directionDPSThreshold = 10; //<----This needs to be set!
+   private ArrayList<Boolean> directionDetections = new ArrayList<Boolean>();
+   private int directionDetectionsLimit = 50;
+
+   //Delete these after
    private boolean mounted;
    private boolean balanced;
-
-   private int engageStage = 0;
 
    public EngageChargingStation(DriveTrain m_DriveTrain, XboxController m_XboxController) {
       this.driveTrain = m_DriveTrain;
@@ -44,27 +61,15 @@ public class EngageChargingStation extends CommandBase {
 
    @Override
    public void execute() {
-      update();
-      if (!mounted && engageStage == 0) {
-         mount();
-      } else if (mounted && engageStage == 0) {
-         engageStage = 1;
-         System.out.println("Stage 1 done!");
-      }
-
-      if (!balanced && engageStage == 1) {
-         balance();
-      } else {
-         System.out.println("Stage 2 done!");
-      }
+      
    }
 
    public void test() {
       if (xbox.getYButton()) {
          update();
-         // if (zSample.size() == zSampleSize || (zSample.size() > zSampleSize)) {
-         //    System.out.println(zAcc);
-         // }
+         System.out.println(mounted + " " + mountDPS); //1
+         // System.out.println(yAcc); //2
+         // System.out.println(balanced + " " + balanceDPS);//3
       }
       if (xbox.getBButton()) {
          update();
@@ -82,57 +87,70 @@ public class EngageChargingStation extends CommandBase {
 
    // Adjusts robot to balance and engage on the charging station
    private void balance() {
-      if (!(zAcc >= targetBalance - thresholdRange && zAcc <= targetBalance + thresholdRange)) {
-         driveTrain.arcadeDrive((targetBalance - zAcc) * 12, 0);
-         balanced = false;
-      } else {
-         balanced = true;
+      if (balanceDPS <= balanceDPSThreshold){
          System.out.println("Balanced!");
+      } else {
+         //Direction code!
+         System.out.println("Not Balanced!");
       }
    }
 
    // Moves robot to sit on the incline of the charging station ramp
    private void mount() {
-      int count = 0;
-      for (int i = 0; i < secondMountSample.size()-1; i++){
-         if (secondMountSample.get(i)) count++;
-      }
-
-      if (count >= 2) {
-         System.out.println("Mounted! " + secondMountSample.toString());
+      if (mountDPS >= mountDPSThreshold){
+         System.out.println("Mounted!");
       } else {
          driveTrain.arcadeDrive(Constants.CHARGE_DRIVE_SPEED, 0);
-         System.out.println("not mounted " + secondMountSample.toString());
+         System.out.println("Not Mounted!");
       }
    }
-
+   
+   //Updates all the data the charging station program uses
    private void update() {
       // Uses an average of accelerometer values rather than raw data; this makes the
       // robot move smoother.
       // System.out.println(mountSample.toString());
       zAcc = accelerometer.getZ();
-      if (mountSample.size() >= sampleSize) {
-         mountSample.clear();
-      };
-      if (!(zAcc >= targetIncline - thresholdRange && zAcc <= targetIncline + thresholdRange)) {
-         mounted = false;
-         mountSample.add(false);
+      yAcc = accelerometer.getY();
+
+      //Mounting data collection
+      if (mountDetections.size() == mountDetectionsLimit) mountDetections.remove(0);
+      if (zAcc >= targetIncline - thresholdRange && zAcc <= targetIncline + thresholdRange) {
+         mountDetections.add(true);
+         mounted = true; //Delete this after
       } else {
-         mounted = true;
-         mountSample.add(true);
+         mountDetections.add(false);
+         mounted = false; //Delete this after
+      }
+      mountDPS = 0;
+      for (int i = 0; i < mountDetections.size(); i++){
+         if (mountDetections.get(i)) mountDPS++;
       }
 
-      int count = 0;
-      for (int i = 0; i < mountSample.size()-1; i++){
-         if (mountSample.get(i)) count++;
+      //Balancing data collection
+      if (balanceDetections.size() == balanceDetectionsLimit) balanceDetections.remove(0);
+      if (zAcc >= targetBalance - thresholdRange && zAcc <= targetBalance + thresholdRange) {
+         balanceDetections.add(true);
+         balanced = true; //Delete this after
+      } else {
+         balanceDetections.add(false);
+         balanced = false; //Delete this after
+      }
+      balanceDPS = 0;
+      for (int i = 0; i < balanceDetections.size(); i++){
+         if (balanceDetections.get(i)) balanceDPS++;
       }
 
-      if (secondMountSample.size() == 3) secondMountSample.remove(0);
-      if (count >= Math.round(mountSample.size()/2)){
-         secondMountSample.add(true);
+      //Direction sensing data collection
+      if (directionDetections.size() == directionDetectionsLimit) directionDetections.remove(0);
+      if (yAcc >= 0) {
+         directionDetections.add(true);
       } else {
-         secondMountSample.add(false);
+         directionDetections.add(false);
       }
-      System.out.println(secondMountSample.toString());
+      directionDPS = 0;
+      for (int i = 0; i < directionDetections.size(); i++){
+         if (directionDetections.get(i)) directionDPS++;
+      }
    }
 }
